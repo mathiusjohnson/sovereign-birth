6947c9257552f2a6c34a9c72a7775f4eb3a3ee06
Mathius <devachievem@gmail.com>
2021-02-23 15:37:25 -0800

    implement emailjs and contact form with reset on submit

--- src/components/Contact/EmailInput.jsx
+++ src/components/Contact/EmailInput.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const EmailInput = () => {
+const EmailInput = ({ onEmailChange , email}) => {
   return (
     <div className="flex flex-wrap -mx-3 mb-6">
       <div className="w-full px-3">
         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
           E-mail
         </label>
-        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email"></input>
+        <input value={email} onChange={(event) => onEmailChange(event)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email"></input>
         <p className="text-gray-600 text-xs italic">Some tips - as long as needed</p>
       </div>
     </div>
