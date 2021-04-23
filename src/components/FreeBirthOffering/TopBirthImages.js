5b72d0fbabebd31c93fd2710a6f281c56f51ba7b
Mathius <devachievem@gmail.com>
2021-04-23 13:09:02 -0700

    implement free birth offering page

--- src/components/FreeBirthOffering/TopBirthImages.js
+++ src/components/FreeBirthOffering/TopBirthImages.js
@@ -3,10 +3,10 @@ import React from 'react';
 const TopBirthImages = () => {
   return (
     <div className="flex">
-      <img className="w-1/4" src="https://picsum.photos/400/275" alt="picsum"></img>
-      <img className="w-1/4" src="https://picsum.photos/400/275" alt="picsum"></img>      
-      <img className="w-1/4" src="https://picsum.photos/400/275" alt="picsum"></img>
-      <img className="w-1/4" src="https://picsum.photos/400/275" alt="picsum"></img>
+      <img className="w-1/4 transition transform hover:scale-105 overflow-hidden" src="https://picsum.photos/400/275" alt="picsum"></img>
+      <img className="w-1/4 transition transform hover:scale-105" src="https://picsum.photos/400/275" alt="picsum"></img>      
+      <img className="w-1/4 transition transform hover:scale-105" src="https://picsum.photos/400/275" alt="picsum"></img>
+      <img className="w-1/4 transition transform hover:scale-105" src="https://picsum.photos/400/275" alt="picsum"></img>
     </div>
   );
 };
