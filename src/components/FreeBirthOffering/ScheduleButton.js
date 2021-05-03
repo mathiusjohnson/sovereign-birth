38bc0bf67408be2f74b2962048d8fba4e4d6a8d8
Mathius <devachievem@gmail.com>
2021-05-03 12:22:17 -0700

    adjust layout of offerings

--- src/components/FreeBirthOffering/ScheduleButton.js
+++ src/components/FreeBirthOffering/ScheduleButton.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ScheduleButton = () => {
   return (
     <div>
-      <Link to="/contact" className="btn btn-primary uppercase">Schedule a Free Connection Call!</Link>
+      <Link to="/contact" className="btn btn-primary uppercase text-base md:text-2xl">Schedule a Free Connection Call!</Link>
     </div>
   );
 };
--- src/components/Services/ServiceListItem/ServiceButton.js
+++ src/components/Services/ServiceListItem/ServiceButton.js
@@ -5,13 +5,13 @@ const ServiceButton = ({onClickSaveState, call_to_action, call_to_action_link, c
   console.log();
   if (call_to_action_target === '_blank') {
     return (
-      <a href={call_to_action_link} target={call_to_action_target} rel="noreferrer"  className="btn btn-primary leading-5 m-2  md:m-4">
+      <a href={call_to_action_link} target={call_to_action_target} rel="noreferrer"  className="btn btn-primary text-base md:text-2xl leading-5 m-2  md:m-4">
         {call_to_action}
       </a>
     );
   } else {
     return (
-      <Link onClick={() => onClickSaveState()} to={call_to_action_link} rel="noreferrer" className="btn btn-primary leading-5 m-2  md:m-4">
+      <Link onClick={() => onClickSaveState()} to={call_to_action_link} rel="noreferrer" className="btn btn-primary text-base md:text-2xl leading-5 m-2  md:m-4">
         {call_to_action}
       </Link>
     );
